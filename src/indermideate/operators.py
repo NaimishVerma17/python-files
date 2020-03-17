21214c1a8c677d83c08ccfd110e2a538f3b9267e
naimishverma17 <itakgec@gmail.com>
2020-03-17 12:42:17 +0530

    operators added

---
+++ src/indermideate/operators.py
@@ -0,0 +1,25 @@
+print(4 + True)
+
+print('String' + str(4))
+
+var1 = 162
+var2 = 5
+print(var1 / var2)  # Precise value
+print(var1 // var2)  # Exact value
+print(var1 % var2)  # Reminder
+
+var3 = True
+var4 = False
+
+print(var3 and var4)
+print(var3 or var4)
+
+var5 = [1, 2, 3, 4]
+print(5 in var5)
+print(id(var5))
+
+var6 = 20
+var7 = 20
+# Points to same identity
+print(id(var6))
+print(id(var7))
